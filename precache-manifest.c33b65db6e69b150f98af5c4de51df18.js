736c005821a0f4520aa9e8668cb98a5f44c59dd6
Łukasz Grygier <lukaszgrygier@icloud.com>
2019-07-16 09:22:27 +0200

    Updates

---
+++ precache-manifest.c33b65db6e69b150f98af5c4de51df18.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "573d72f4f3d61c964264c975643201e6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b9e54f7d90cb1fe4f1ce",
+    "url": "/portfolio/static/css/main.13a273e8.chunk.css"
+  },
+  {
+    "revision": "53d4007fa588994e8acd",
+    "url": "/portfolio/static/js/2.f8d5c18a.chunk.js"
+  },
+  {
+    "revision": "b9e54f7d90cb1fe4f1ce",
+    "url": "/portfolio/static/js/main.5bf736bb.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "25bf4aa61552d612fcceb1d450edc061",
+    "url": "/portfolio/static/media/1.25bf4aa6.jpg"
+  },
+  {
+    "revision": "272a394d12bbce3713e0ed8e0aba277a",
+    "url": "/portfolio/static/media/2.272a394d.jpg"
+  },
+  {
+    "revision": "237b2123f7244269983f1c0e6613ed5c",
+    "url": "/portfolio/static/media/3.237b2123.jpg"
+  },
+  {
+    "revision": "0d02a85d6c53fe663f2641c002c8a0d4",
+    "url": "/portfolio/static/media/4.0d02a85d.jpg"
+  },
+  {
+    "revision": "17957e70b95f6b3de2d6ff4f3f054ef4",
+    "url": "/portfolio/static/media/5.17957e70.jpg"
+  },
+  {
+    "revision": "94a34872458945ededf044496ea2af0c",
+    "url": "/portfolio/static/media/Detal.94a34872.jpg"
+  },
+  {
+    "revision": "91dd43d4caf3c9022534b94cf653f0ea",
+    "url": "/portfolio/static/media/Elewacja.91dd43d4.jpg"
+  },
+  {
+    "revision": "166ff57ecfda9934baaf91f2e18474bd",
+    "url": "/portfolio/static/media/Plan,detal.166ff57e.jpg"
+  },
+  {
+    "revision": "2d9b24b928eccb124955f65aaded2c23",
+    "url": "/portfolio/static/media/Plan,przekrój.2d9b24b9.jpg"
+  },
+  {
+    "revision": "51b6aa4f514b484eb5424aea43be4a96",
+    "url": "/portfolio/static/media/Plan,przekrój.51b6aa4f.jpg"
+  },
+  {
+    "revision": "5fb55a5755144072ab3fa9058059470f",
+    "url": "/portfolio/static/media/Plan,przekrój.5fb55a57.jpg"
+  },
+  {
+    "revision": "9067ef44327ddc3d0694420140605da8",
+    "url": "/portfolio/static/media/Plan,przekrój.9067ef44.jpg"
+  },
+  {
+    "revision": "a138eabe61686e9a061b2fcba5687d41",
+    "url": "/portfolio/static/media/Plan,przekrój.a138eabe.jpg"
+  },
+  {
+    "revision": "3382cb54d188fb7cf97871e7f7b022c7",
+    "url": "/portfolio/static/media/Plan,przekrój2.3382cb54.jpg"
+  },
+  {
+    "revision": "4f2d236de05fc574f4730ab8a455d4cf",
+    "url": "/portfolio/static/media/Plan,przekrój2.4f2d236d.jpg"
+  },
+  {
+    "revision": "9da9a01238877bac979a8a01291fb772",
+    "url": "/portfolio/static/media/Plan,przekrój3.9da9a012.jpg"
+  },
+  {
+    "revision": "f0def8540a6e99639b3f73b52bc873af",
+    "url": "/portfolio/static/media/Plan.f0def854.jpg"
+  },
+  {
+    "revision": "9155df789958a40cdb1bb0d035428b5a",
+    "url": "/portfolio/static/media/Przekrój.9155df78.jpg"
+  },
+  {
+    "revision": "87997e400d2d2926f2ef34e1282c5b28",
+    "url": "/portfolio/static/media/Wizualizacja.87997e40.jpg"
+  },
+  {
+    "revision": "9ecfc4e39b10f1f249d941e657bffad5",
+    "url": "/portfolio/static/media/Wizualizacja.9ecfc4e3.jpg"
+  },
+  {
+    "revision": "bd5e670cf9ac5ce467789b3c1959ddbc",
+    "url": "/portfolio/static/media/Wizualizacja.bd5e670c.jpg"
+  },
+  {
+    "revision": "f64138ff9ec4d527909d7f72cd244ffe",
+    "url": "/portfolio/static/media/Wizualizacja.f64138ff.jpg"
+  },
+  {
+    "revision": "ff7c120bea1ec9315d515b5dee2f2145",
+    "url": "/portfolio/static/media/Wizualizacja.ff7c120b.jpg"
+  },
+  {
+    "revision": "8e97d8195c0b99b4ed4e043ff4aa8d3a",
+    "url": "/portfolio/static/media/Wizualizacja2.8e97d819.jpg"
+  },
+  {
+    "revision": "932724249d6cff2a079fd9b93e7a7fe9",
+    "url": "/portfolio/static/media/Wizualizacja2.93272424.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
